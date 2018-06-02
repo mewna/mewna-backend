e89d8fed34229b56e09ba3d6ab86d85198b71f88
amy null <null@amy.gg>
2018-06-02 15:00:32 -0400

    Rank cards generate now

--- src/main/java/com/mewna/plugin/plugins/PluginLevels.java
+++ src/main/java/com/mewna/plugin/plugins/PluginLevels.java
@@ -1,5 +1,6 @@
 package com.mewna.plugin.plugins;
 
+import com.mewna.Mewna;
 import com.mewna.cache.entity.Guild;
 import com.mewna.cache.entity.Member;
 import com.mewna.cache.entity.User;
@@ -13,8 +14,9 @@ import com.mewna.plugin.event.EventType;
 import com.mewna.plugin.event.message.MessageCreateEvent;
 import com.mewna.plugin.event.plugin.levels.LevelUpEvent;
 import com.mewna.plugin.plugins.settings.LevelsSettings;
+import com.mewna.plugin.util.CardGenerator;
 import com.mewna.util.Templater;
-import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.MessageBuilder;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.json.JSONObject;
 
@@ -33,15 +35,15 @@ import java.util.stream.Collectors;
 @SuppressWarnings({"unused", "WeakerAccess"})
 @Plugin(name = "Levels", desc = "Allow gaining xp and leveling up by chatting.", settings = LevelsSettings.class)
 public class PluginLevels extends BasePlugin {
-    static boolean isLevelUp(final long oldXp, final long xp) {
+    public static boolean isLevelUp(final long oldXp, final long xp) {
         return xpToLevel(oldXp) < xpToLevel(xp);
     }
     
-    static long levelToXp(final long level) {
+    public static long levelToXp(final long level) {
         return Math.max(0, 100 * level + 20 * (level - 1));
     }
     
-    static long fullLevelToXp(final long level) {
+    public static long fullLevelToXp(final long level) {
         long requiredXp = 0;
         for(int i = 1; i <= level; i++) {
             requiredXp += levelToXp(i);
@@ -49,11 +51,11 @@ public class PluginLevels extends BasePlugin {
         return requiredXp;
     }
     
-    static long nextLevelXp(final long xp) {
+    public static long nextLevelXp(final long xp) {
         return fullLevelToXp(xpToLevel(xp) + 1) - xp;
     }
     
-    static long xpToLevel(long xp) {
+    public static long xpToLevel(long xp) {
         long level = 0;
         while(xp >= levelToXp(level)) {
             xp -= levelToXp(level);
@@ -61,6 +63,37 @@ public class PluginLevels extends BasePlugin {
         }
         
         return Math.max(0, level - 1);
+    }
+    
+    public static int getAllRankedPlayersInGuild(final Guild guild) {
+        final int[] count = {0};
+        Mewna.getInstance().getDatabase().getStore().sql("SELECT COUNT(*) AS count FROM players WHERE data->'guildXp'->'" + guild.getId() + "' IS NOT NULL;", p -> {
+            final ResultSet resultSet = p.executeQuery();
+            if(resultSet.isBeforeFirst()) {
+                resultSet.next();
+            }
+            count[0] = resultSet.getInt("count");
+        });
+        return count[0];
+    }
+    
+    public static int getPlayerRankInGuild(final Guild guild, final User player) {
+        final int[] rank = {-1};
+        final String guildId = guild.getId();
+        final String playerId = player.getId();
+        Mewna.getInstance().getDatabase().getStore().sql("SELECT rank FROM (SELECT row_number() OVER () AS rank, data FROM players " +
+                "WHERE data->'guildXp'->'" + guildId + "' IS NOT NULL " +
+                "ORDER BY (data->'guildXp'->>'" + guildId + "')::integer DESC) AS _q " +
+                "WHERE data->>'id' = '" + playerId + "';", p -> {
+            final ResultSet resultSet = p.executeQuery();
+            if(resultSet.isBeforeFirst()) {
+                resultSet.next();
+                rank[0] = resultSet.getInt("rank");
+            } else {
+                rank[0] = 1;
+            }
+        });
+        return rank[0];
     }
     
     private Templater map(final LevelUpEvent event) {
@@ -184,32 +217,13 @@ public class PluginLevels extends BasePlugin {
             getRestJDA().sendMessage(ctx.getChannel(), "Levels are not enabled in this server.").queue();
             return;
         }
-        final long xp;
-        final long level;
-        final String name;
-        final String discrim;
-        final String avatar;
-        if(ctx.getMentions().isEmpty()) {
-            xp = ctx.getPlayer().getXp(guild);
-            name = ctx.getUser().getName();
-            discrim = ctx.getUser().getDiscriminator();
-            avatar = ctx.getUser().getAvatarURL();
-        } else {
-            final User user = ctx.getMentions().get(0);
-            final Player player = getDatabase().getPlayer(user);
-            xp = player.getXp(guild);
-            name = user.getName();
-            discrim = user.getDiscriminator();
-            avatar = user.getAvatarURL();
-        }
-        level = xpToLevel(xp);
-        final EmbedBuilder builder = new EmbedBuilder().setTitle(String.format("%s#%s's rank", name, discrim))
-                .addField("Level", "Level " + level, true).addBlankField(true)
-                .addField("XP", xp + " / " + (fullLevelToXp(xpToLevel(xp)) + levelToXp(xpToLevel(xp) + 1)) + " (" + nextLevelXp(xp) + "xp left)", true)
-                .addField("Rank", getPlayerRankInGuild(guild, ctx.getUser()) + " / "
-                        + getAllRankedPlayersInGuild(guild), true)
-                .setThumbnail(avatar);
-        getRestJDA().sendMessage(ctx.getChannel(), builder.build()).queue();
+        
+        getRestJDA().sendTyping(ctx.getChannel()).queue(__ -> {
+            final byte[] cardBytes = CardGenerator.generateRankCard(ctx.getGuild(), ctx.getUser(), ctx.getPlayer());
+            getRestJDA().sendFile(ctx.getChannel(), cardBytes, "rank.png",
+                    new MessageBuilder().append("**").append(ctx.getUser().getName()).append("**'s rank card").build())
+                    .queue();
+        });
     }
     
     @Command(names = {"leaderboards", "ranks", "levels", "leaderboard", "rankings"}, desc = "View the guild leaderboards.",
@@ -222,37 +236,6 @@ public class PluginLevels extends BasePlugin {
             return;
         }
         getRestJDA().sendMessage(ctx.getChannel(), "https://amy.chat/leaderboards/" + guild.getId()).queue();
-    }
-    
-    private int getAllRankedPlayersInGuild(final Guild guild) {
-        final int[] count = {0};
-        getDatabase().getStore().sql("SELECT COUNT(*) AS count FROM players WHERE data->'guildXp'->'" + guild.getId() + "' IS NOT NULL;", p -> {
-            final ResultSet resultSet = p.executeQuery();
-            if(resultSet.isBeforeFirst()) {
-                resultSet.next();
-            }
-            count[0] = resultSet.getInt("count");
-        });
-        return count[0];
-    }
-    
-    private int getPlayerRankInGuild(final Guild guild, final User player) {
-        final int[] rank = {-1};
-        final String guildId = guild.getId();
-        final String playerId = player.getId();
-        getDatabase().getStore().sql("SELECT rank FROM (SELECT row_number() OVER () AS rank, data FROM players " +
-                "WHERE data->'guildXp'->'" + guildId + "' IS NOT NULL " +
-                "ORDER BY (data->'guildXp'->>'" + guildId + "')::integer DESC) AS _q " +
-                "WHERE data->>'id' = '" + playerId + "';", p -> {
-            final ResultSet resultSet = p.executeQuery();
-            if(resultSet.isBeforeFirst()) {
-                resultSet.next();
-                rank[0] = resultSet.getInt("rank");
-            } else {
-                rank[0] = 1;
-            }
-        });
-        return rank[0];
     }
     
     private long getXp(final Player player) {
---
+++ src/main/java/com/mewna/plugin/util/CardGenerator.java
@@ -0,0 +1,178 @@
+package com.mewna.plugin.util;
+
+import com.mewna.cache.entity.Guild;
+import com.mewna.cache.entity.User;
+import com.mewna.data.Player;
+import com.mewna.plugin.plugins.PluginLevels;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.font.TextAttribute;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Generates the cards for rank, level up, ...
+ *
+ * @author amy
+ * @since 6/2/18.
+ */
+@SuppressWarnings("WeakerAccess")
+public final class CardGenerator {
+    // colours
+    
+    public static final String PRIMARY_COLOUR_STR = "0xdb325c";
+    @SuppressWarnings("unused")
+    public static final int PRIMARY_COLOUR = 0xdb325c;
+    private static final Color NINETY_PERCENT_OPAQUE_BLACK = new Color(0, 0, 0, 230);
+    private static final Color SIXTY_SEVEN_PERCENT_OPAQUE_BLACK = new Color(0, 0, 0, 171);
+    private static final Color PRIMARY_THEME_COLOUR = Color.decode(PRIMARY_COLOUR_STR);
+    
+    // fonts
+    
+    private static final Font USERNAME_FONT;
+    private static final Font STATS_FONT;
+    private static final Font STATS_FONT_SMALLER;
+    
+    private static final Map<TextAttribute, Object> FONT_SETTINGS = new HashMap<>();
+    
+    static {
+        FONT_SETTINGS.put(TextAttribute.KERNING, TextAttribute.KERNING_ON);
+    
+        USERNAME_FONT = new Font("Yanone Kaffeesatz", Font.PLAIN, 42).deriveFont(FONT_SETTINGS);
+        STATS_FONT = new Font("Droid Sans", Font.PLAIN, 32).deriveFont(FONT_SETTINGS);
+        STATS_FONT_SMALLER = new Font("Droid Sans", Font.PLAIN, 30).deriveFont(FONT_SETTINGS);
+        System.clearProperty("swing.defaultlaf");
+    }
+    
+    private CardGenerator() {
+    }
+    
+    private static void setRenderHints(final Graphics2D g2) {
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED);
+    }
+    
+    @SuppressWarnings("unused")
+    public static byte[] generateRankCard(final Guild guild, final User user, final Player player) { // lol
+        final BufferedImage card = new BufferedImage(800, 200, BufferedImage.TYPE_INT_ARGB);
+        // TODO: Custom backgrounds
+        try {
+            final Graphics2D g2 = card.createGraphics();
+            setRenderHints(g2);
+            // Background
+            g2.setColor(PRIMARY_THEME_COLOUR);
+            g2.fillRect(0, 0, 800, 200);
+            
+            // Main card panel
+            g2.setColor(NINETY_PERCENT_OPAQUE_BLACK);
+            g2.fillRect(100, 10, 676, 180);
+            
+            // Avatar panel
+            g2.setColor(SIXTY_SEVEN_PERCENT_OPAQUE_BLACK);
+            
+            g2.fillRect(34, 34, 132, 132);
+            
+            // Avatar
+            final BufferedImage avatar = downloadAvatar(user.getAvatarURL().replaceAll("gif", "png") + "?size=128");
+            g2.drawImage(avatar, 36, 36, 128, 128, null);
+            
+            // Username
+            g2.setPaint(Color.WHITE);
+            g2.setFont(USERNAME_FONT);
+            setRenderHints(g2);
+            g2.drawString(user.getName(), 187, 70);
+            
+            // Stats
+            
+            final String lvl = "LVL ";
+            final String rank = "RANK #";
+            
+            g2.setFont(STATS_FONT);
+            setRenderHints(g2);
+            
+            // User stats
+            final long userXp = player.getXp(guild);
+            final long userLevel = PluginLevels.xpToLevel(userXp);
+            final long nextLevel = userLevel + 1;
+            // Font sizing util
+            final FontMetrics metrics = g2.getFontMetrics(STATS_FONT);
+            final int lvlWidth = metrics.stringWidth(lvl);
+            final int rankWidth = metrics.stringWidth(rank);
+            final int playerLevelWidth = metrics.stringWidth(userLevel + "    ");
+            final long playerRank = PluginLevels.getPlayerRankInGuild(guild, user);
+            
+            g2.drawString(lvl, 187, 113); // LVL
+            g2.setPaint(PRIMARY_THEME_COLOUR);
+            g2.drawString(userLevel + "    ", 187 + lvlWidth, 113); // 1234
+            g2.setPaint(Color.WHITE);
+            g2.drawString(rank, 187 + lvlWidth + playerLevelWidth, 113); // RANK #
+            g2.setPaint(PRIMARY_THEME_COLOUR);
+            g2.drawString(playerRank + "", 187 + lvlWidth + playerLevelWidth + rankWidth, 113); // 123456
+            
+            // XP bar
+            final long currentLevelXp = PluginLevels.fullLevelToXp(userLevel);
+            final long nextLevelXp = PluginLevels.fullLevelToXp(nextLevel);
+            final long xpNeeded = PluginLevels.nextLevelXp(userXp);
+            final long nextXpTotal = nextLevelXp - currentLevelXp - xpNeeded;
+            g2.setColor(SIXTY_SEVEN_PERCENT_OPAQUE_BLACK);
+            g2.fillRect(188, 123, 566, 42);
+            // calc. bar size
+            final int barWidth = (int) (562 * (nextXpTotal / (double) nextLevelXp));
+            g2.setColor(PRIMARY_THEME_COLOUR);
+            g2.fillRect(190, 125, barWidth, 38);
+            // XP text
+            drawCenteredString(g2, String.format("%s / %s EXP", nextXpTotal, nextLevelXp),
+                    new Rectangle(190, 125, 562, 38), STATS_FONT_SMALLER, Color.WHITE);
+            
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(card, "png", baos);
+            g2.dispose();
+            final byte[] bytes = baos.toByteArray();
+            baos.close();
+            return bytes;
+        } catch(final IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+    
+    private static void drawCenteredString(final Graphics2D g2, final String text, final Rectangle rect,
+                                           @SuppressWarnings("SameParameterValue") final Font font,
+                                           @SuppressWarnings("SameParameterValue") final Color color) {
+        final FontMetrics metrics = g2.getFontMetrics(font);
+        final int x = rect.x + (rect.width - metrics.stringWidth(text)) / 2;
+        final int y = rect.y + (rect.height - metrics.getHeight()) / 2 + metrics.getAscent();
+        g2.setFont(font);
+        setRenderHints(g2);
+        g2.setColor(color);
+        g2.drawString(text, x, y);
+    }
+    
+    private static BufferedImage downloadAvatar(final String avatarUrl) {
+        try {
+            final URL url = new URL(avatarUrl);
+            final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // Default Java user agent gets blocked by Discord :(
+            connection.setRequestProperty(
+                    "User-Agent",
+                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " +
+                            "Chrome/55.0.2883.87 Safari/537.36");
+            final InputStream is = connection.getInputStream();
+            final BufferedImage avatar = ImageIO.read(is);
+            is.close();
+            connection.disconnect();
+            return avatar;
+        } catch(final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

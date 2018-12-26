5f486dce02639c5fae1b75866178ed4e210b92b7
amy null <null@amy.gg>
2018-12-26 08:43:42 -0500

    aaaaaaaaaaaaaaaaaaaaa-rc2

--- src/main/java/com/mewna/event/SingyeongEventManager.java
+++ src/main/java/com/mewna/event/SingyeongEventManager.java
@@ -17,10 +17,7 @@ import com.mewna.plugin.event.plugin.twitch.TwitchStreamEndEvent;
 import com.mewna.plugin.event.plugin.twitch.TwitchStreamStartEvent;
 import com.mewna.plugin.event.plugin.twitch.TwitchStreamer;
 import gg.amy.singyeong.Dispatch;
-import io.vertx.core.WorkerExecutor;
 import io.vertx.core.json.JsonObject;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author amy
@@ -28,76 +25,66 @@ import java.util.concurrent.TimeUnit;
  */
 public class SingyeongEventManager {
     private final Mewna mewna;
-    private final WorkerExecutor workerExecutor;
     
     public SingyeongEventManager(final Mewna mewna) {
         this.mewna = mewna;
-        
-        workerExecutor = mewna.vertx().createSharedWorkerExecutor("mewna-event-processing-pool",
-                Runtime.getRuntime().availableProcessors() * 10, 5, TimeUnit.MINUTES);
     }
     
     public void handle(final Dispatch dispatch) {
-        
-        
-        workerExecutor.executeBlocking(f -> {
-            final JsonObject data = dispatch.data();
-            if(data.containsKey("type") && dispatch.nonce() == null) {
-                final String type = data.getString("type");
-                switch(type) {
-                    case Raw.MESSAGE_CREATE: {
-                        final var event = DiscordMessageCreate.builder()
-                                .guild(Entity.fromJson(mewna.catnip(), GuildImpl.class, data.getJsonObject("guild")))
-                                .member(Entity.fromJson(mewna.catnip(), MemberImpl.class, data.getJsonObject("member")))
-                                .user(Entity.fromJson(mewna.catnip(), UserImpl.class, data.getJsonObject("user")))
-                                .message(Entity.fromJson(mewna.catnip(), MessageImpl.class, data.getJsonObject("message")))
-                                .build();
-                        mewna.commandManager().tryExecCommand(event);
-                        break;
-                    }
-                    case Raw.GUILD_MEMBER_ADD: {
-                        final var event = DiscordGuildMemberAdd.builder()
-                                .guild(Entity.fromJson(mewna.catnip(), GuildImpl.class, data.getJsonObject("guild")))
-                                .member(Entity.fromJson(mewna.catnip(), MemberImpl.class, data.getJsonObject("member")))
-                                .user(Entity.fromJson(mewna.catnip(), UserImpl.class, data.getJsonObject("user")))
-                                .build();
-                        mewna.pluginManager().processEvent(type, event);
-                        break;
-                    }
-                    case Raw.GUILD_MEMBER_REMOVE: {
-                        final var event = DiscordGuildMemberRemove.builder()
-                                .guild(Entity.fromJson(mewna.catnip(), GuildImpl.class, data.getJsonObject("guild")))
-                                .member(Entity.fromJson(mewna.catnip(), MemberImpl.class, data.getJsonObject("member")))
-                                .user(Entity.fromJson(mewna.catnip(), UserImpl.class, data.getJsonObject("user")))
-                                .build();
-                        mewna.pluginManager().processEvent(type, event);
-                        break;
-                    }
-                    case EventType.AUDIO_TRACK_NO_MATCHES:
-                    case EventType.AUDIO_TRACK_QUEUE_MANY:
-                    case EventType.AUDIO_TRACK_NOW_PLAYING:
-                    case EventType.AUDIO_TRACK_START:
-                    case EventType.AUDIO_QUEUE_END:
-                    case EventType.AUDIO_TRACK_QUEUE: {
-                        final var event = data.getJsonObject("data").mapTo(NekoTrackEvent.class);
-                        mewna.pluginManager().processEvent(type, event);
-                        break;
-                    }
-                    case EventType.TWITCH_STREAM_START: {
-                        final var streamer = data.getJsonObject("data").getJsonObject("streamer").mapTo(TwitchStreamer.class);
-                        final var stream = data.getJsonObject("data").getJsonObject("streamData").mapTo(TwitchStreamData.class);
-                        mewna.pluginManager().processEvent(type, new TwitchStreamStartEvent(streamer, stream));
-                        break;
-                    }
-                    case EventType.TWITCH_STREAM_END: {
-                        final var streamer = data.getJsonObject("data").getJsonObject("streamer").mapTo(TwitchStreamer.class);
-                        mewna.pluginManager().processEvent(type, new TwitchStreamEndEvent(streamer));
-                        break;
-                    }
+        final JsonObject data = dispatch.data();
+        if(data.containsKey("type") && dispatch.nonce() == null) {
+            final String type = data.getString("type");
+            switch(type) {
+                case Raw.MESSAGE_CREATE: {
+                    final var event = DiscordMessageCreate.builder()
+                            .guild(Entity.fromJson(mewna.catnip(), GuildImpl.class, data.getJsonObject("guild")))
+                            .member(Entity.fromJson(mewna.catnip(), MemberImpl.class, data.getJsonObject("member")))
+                            .user(Entity.fromJson(mewna.catnip(), UserImpl.class, data.getJsonObject("user")))
+                            .message(Entity.fromJson(mewna.catnip(), MessageImpl.class, data.getJsonObject("message")))
+                            .build();
+                    mewna.commandManager().tryExecCommand(event);
+                    break;
+                }
+                case Raw.GUILD_MEMBER_ADD: {
+                    final var event = DiscordGuildMemberAdd.builder()
+                            .guild(Entity.fromJson(mewna.catnip(), GuildImpl.class, data.getJsonObject("guild")))
+                            .member(Entity.fromJson(mewna.catnip(), MemberImpl.class, data.getJsonObject("member")))
+                            .user(Entity.fromJson(mewna.catnip(), UserImpl.class, data.getJsonObject("user")))
+                            .build();
+                    mewna.pluginManager().processEvent(type, event);
+                    break;
+                }
+                case Raw.GUILD_MEMBER_REMOVE: {
+                    final var event = DiscordGuildMemberRemove.builder()
+                            .guild(Entity.fromJson(mewna.catnip(), GuildImpl.class, data.getJsonObject("guild")))
+                            .member(Entity.fromJson(mewna.catnip(), MemberImpl.class, data.getJsonObject("member")))
+                            .user(Entity.fromJson(mewna.catnip(), UserImpl.class, data.getJsonObject("user")))
+                            .build();
+                    mewna.pluginManager().processEvent(type, event);
+                    break;
+                }
+                case EventType.AUDIO_TRACK_NO_MATCHES:
+                case EventType.AUDIO_TRACK_QUEUE_MANY:
+                case EventType.AUDIO_TRACK_NOW_PLAYING:
+                case EventType.AUDIO_TRACK_START:
+                case EventType.AUDIO_QUEUE_END:
+                case EventType.AUDIO_TRACK_QUEUE: {
+                    final var event = data.getJsonObject("data").mapTo(NekoTrackEvent.class);
+                    mewna.pluginManager().processEvent(type, event);
+                    break;
+                }
+                case EventType.TWITCH_STREAM_START: {
+                    final var streamer = data.getJsonObject("data").getJsonObject("streamer").mapTo(TwitchStreamer.class);
+                    final var stream = data.getJsonObject("data").getJsonObject("streamData").mapTo(TwitchStreamData.class);
+                    mewna.pluginManager().processEvent(type, new TwitchStreamStartEvent(streamer, stream));
+                    break;
+                }
+                case EventType.TWITCH_STREAM_END: {
+                    final var streamer = data.getJsonObject("data").getJsonObject("streamer").mapTo(TwitchStreamer.class);
+                    mewna.pluginManager().processEvent(type, new TwitchStreamEndEvent(streamer));
+                    break;
                 }
             }
-            
-            f.complete(null);
-        }, res -> {});
+        }
     }
 }
